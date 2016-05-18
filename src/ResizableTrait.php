dd3fe1a9f90e530828da596369aa228fa34627dd
Carlo <c.catemario@gmail.com>
2016-05-18 10:55:48 +0100

    test the tests

--- src/ResizableTrait.php
+++ src/ResizableTrait.php
@@ -5,8 +5,18 @@ namespace Keisen\Resizable;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Intervention\Image\ImageManager;
 
-trait ResizableTrait {
+trait ResizableTrait
+{
 
+    /**
+     * Resize the image to the specified formats and save the filename in
+     * the object property
+     *
+     * @param UploadedFile $file   file
+     * @param string|null  $folder folder
+     *
+     * @return void
+     */
     public function resize(UploadedFile $file, string $folder = null)
     {
         if ($this->hasFormats()) {
@@ -18,38 +28,81 @@ trait ResizableTrait {
             $manager = new ImageManager();
             $image = $manager->make($file);
 
-            $output = $image->$filter($formats[$format][$filter][0],$formats[$format][$filter][1]);
+            $output = $image->$filter(
+                $formats[$format][$filter][0],
+                $formats[$format][$filter][1]
+            );
 
-            $filename = $this->generateName($file);
-            $output->save($folder ."/". $filename );
+            $column = $this->getColumnName();
+            $this->$column = $this->generateName($file);
+
+            $output->save($folder ."/". $this->$column);
         }
     }
 
+    /**
+     * Check if there are formats specified, otherwise fallback
+     *
+     * @return bool
+     */
     public function hasFormats() : bool
     {
-        if (isset($this->resizable['formats']) &&
-            !empty($this->resizable['formats'])) {
+        if (isset($this->resizable['formats'])
+            && !empty($this->resizable['formats'])
+        ) {
             return true;
         }
 
         return false;
     }
-    
+
+    /**
+     * Get the formats from the object property
+     *
+     * @return null
+     */
     public function getFormats()
     {
-        if(isset($this->resizable['formats']) &&
-            is_array($this->resizable['formats'])) {
+        if (isset($this->resizable['formats'])
+            && is_array($this->resizable['formats'])
+        ) {
             return $this->resizable['formats'];
         }
 
         return null;
     }
 
+    /**
+     * Generate a pseudo-unique filename
+     *
+     * @param UploadedFile $file
+     *
+     * @return string
+     */
     public function generateName(UploadedFile  $file) : string
     {
         $ext = "." . $file->getClientOriginalExtension();
-        $filename = preg_replace("/[^a-z0-9\\._-]+/", "", strtolower($file->getClientOriginalName()));
+        $filename = preg_replace(
+            "/[^a-z0-9\\._-]+/",
+            "",
+            strtolower($file->getClientOriginalName())
+        );
+
         return basename($filename, $ext) . "-" . str_random(6) . $ext;
+    }
+
+    /**
+     * Get the column where to store the filename
+     *
+     * @return string
+     */
+    public function getColumnName()
+    {
+        if (isset($this->resizable['column'])) {
+            return $this->resizable['column'];
+        }
+
+        return 'file';
     }
 
 }
--- tests/ResizableTest.php
+++ tests/ResizableTest.php
@@ -4,23 +4,56 @@ namespace Keisen\Resizable\Test;
 
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ResizableTest extends TestCase
 {
+    use DatabaseTransactions;
 
     /**
      * @test
      */
-    public function it_save_the_image_in_default_folder()
+    public function it_saves_the_image_in_folder()
     {
-        $file = new UploadedFile('tests/fixtures/test.jpg', 'test.jpg', null, null, null, true );
+        $file = new UploadedFile(
+            'tests/fixtures/test.jpg',
+            'test.jpg',
+            null,
+            null,
+            null,
+            true
+        );
 
         $image = new Image();
         $image->name = 'test';
         $image->resize($file, 'tests/storage');
-        
-        
 
+        $column = $image->getColumnName();
 
+        $this->assertFileExists('tests/storage/' . $image->$column);
+    }
+
+    /**
+     * @test
+     */
+    public function it_save_the_image_name_in_db()
+    {
+        $file = new UploadedFile(
+            'tests/fixtures/test.jpg',
+            'test.jpg',
+            null,
+            null,
+            null,
+            true
+        );
+
+        $image = new Image();
+        $image->name = 'test';
+        $image->resize($file, 'tests/storage');
+        $image->save();
+
+        $column = $image->getColumnName();
+
+        $this->seeInDatabase('images', [$column => $image->$column]);
     }
 }
